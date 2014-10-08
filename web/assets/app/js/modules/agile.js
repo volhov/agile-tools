c0e2925500ad62a76368319ccf58cccc32b3455b
Max Gopey <max.gopey@symmetrics.de>
2014-10-08 12:16:31 +0300

    - angular-route-segment module has been added (see: https://github.com/artch/angular-route-segment). Todo: - finish the routing. - change the templating logic to be hierarchical.

--- web/assets/app/js/modules/agile.js
+++ web/assets/app/js/modules/agile.js
@@ -1,11 +1,13 @@
 angular.module('agile', [
         'ngRoute',
         'agile.controllers',
-        'agile.filters'
+        'agile.filters',
+        'route-segment',
+        'view-segment'
     ])
     .constant('TEMPLATES_URL', '/assets/app/templates')
-    .config(['$routeProvider', '$locationProvider', '$httpProvider', 'TEMPLATES_URL',
-        function($routeProvider, $locationProvider, $httpProvider, TEMPLATES_URL) {
+    .config(['$routeProvider', '$routeSegmentProvider', '$locationProvider', '$httpProvider', 'TEMPLATES_URL',
+        function($routeProvider, $routeSegmentProvider, $locationProvider, $httpProvider, TEMPLATES_URL) {
 //            $locationProvider.html5Mode(true);
 
 //            $httpProvider.interceptors.push(function($q) {
@@ -16,30 +18,65 @@ angular.module('agile', [
 //                    }
 //                };
 //            });
+            $routeSegmentProvider
+                .when('/start', 'start')
+                    .segment('start', {
+                        templateUrl: TEMPLATES_URL + '/start.html',
+                        controller: 'Start'
+                    })
+                .when('/project/:projectKey', 'project')
+                .when('/version/:projectKey/:versionName', 'project.version')
+                .when('/version/:projectKey/:versionName/:tab', 'project.version.tab')
+                    .segment('project', {
+                        templateUrl: TEMPLATES_URL + '/project.html',
+                        controller: 'Project'
+                    })
+                    .within('project')
+                        .segment('version', {})
+                            .within('version')
+                                .segment('tab', {
+                                    templateUrl: TEMPLATES_URL +'/version.html',
+                                    controller: 'Version',
+                                    dependencies: ['versionName', 'tab']
+                                })
+
+            ;
 
             $routeProvider
-                .when('/start', {
-                    templateUrl: TEMPLATES_URL + '/start.html',
-                    controller: 'Start'
-                })
-                .when('/version/:projectKey/:versionName', {
-                    redirectTo: function(params) {
-                        var defaultTab = 'confidence_report';
-                        return '/version/' + params.projectKey
-                            + '/' + params.versionName + '/' + defaultTab;
-                    }
-                })
-                .when('/version/:projectKey/:versionName/:tab', {
-                    templateUrl: TEMPLATES_URL +'/version.html',
-                    controller: 'Version'
-                })
-                .when('/project/:projectKey', {
-                    templateUrl: TEMPLATES_URL + '/project.html',
-                    controller: 'Project'
-                })
-                .otherwise({
-                    redirectTo: '/start'
-                });
+//                .when('/version/:projectKey/:versionName', {
+//                    redirectTo: function(params) {
+//                        var defaultTab = 'confidence_report';
+//                        return '/version/' + params.projectKey
+//                            + '/' + params.versionName + '/' + defaultTab;
+//                    }
+//                })
+                .otherwise({redirectTo: '/start'});
+
+            /*
+                        $routeProvider
+                            .when('/start', {
+                                templateUrl: TEMPLATES_URL + '/start.html',
+                                controller: 'Start'
+                            })
+                            .when('/version/:projectKey/:versionName', {
+                                redirectTo: function(params) {
+                                    var defaultTab = 'confidence_report';
+                                    return '/version/' + params.projectKey
+                                        + '/' + params.versionName + '/' + defaultTab;
+                                }
+                            })
+                            .when('/version/:projectKey/:versionName/:tab', {
+                                templateUrl: TEMPLATES_URL +'/version.html',
+                                controller: 'Version'
+                            })
+                            .when('/project/:projectKey', {
+                                templateUrl: TEMPLATES_URL + '/project.html',
+                                controller: 'Project'
+                            })
+                            .otherwise({
+                                redirectTo: '/start'
+                            });
+            */
         }]);
 
 angular.module('agile.controllers', ['helper', 'api', 'agile.filters']);
