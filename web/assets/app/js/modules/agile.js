bebbc14ccda68ebcdd08ad4215a7366ef27da6c4
Max Gopey <max.gopey@symmetrics.de>
2014-12-11 16:56:31 +0200

    - Default page changed to /projects

--- web/assets/app/js/modules/agile.js
+++ web/assets/app/js/modules/agile.js
@@ -28,7 +28,7 @@ angular.module('agile', [
                     }
                 })
                 .otherwise({
-                    redirectTo: '/start'
+                    redirectTo: '/projects'
                 });
 
             /**
